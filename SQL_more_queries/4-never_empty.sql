9e712781702774ea497bbba791bde0db281a842e
MarlenaKule <marlenakule1@gmail.com>
2026-09-18 14:28:47 +0200

    task6

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- script that creates the table id_not_null on MySQL server
 CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT UNIQUE DEFAULT 1,
+    id INT UNIQUE NOT NULL DEFAULT 1,
     name VARCHAR(256)
 );
